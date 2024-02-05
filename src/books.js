7e2d3c2ac0a1b1fb786bf817e8c83f3073a2d514
vilnislv <vilnisbeltins@gmail.com>
2024-02-05 16:58:03 +0200

    comm pie

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+  {
+    author: "Sarah J. Maas",
+    title: "House of Flame and Shadow",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Kristin Hannah",
+    title: "The Women",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,37 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-const author = "Sarah J. Maas";
-const title = "House of Flame and Shadow";
-const img = "./images/book-1.jpg";
+import { books } from './books';
+import Book from './Book';
 
-// parameters
-const someFunc = (param1, param2) => {
-  console.log(param1, param2);
-};
-// arguments
-someFunc('job', 'developer');
 const BookList = () => {
   return (
-    <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
+    <>
+      <h1>Amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
-const Book = () => {
-  
-  return (
-    <article className="book">
-      <img src={img} alt={title}/>
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 
 
